96a273fdefcd1527b486b107e50b57a039acae96
Antoni Kepinski <xxczaki@pm.me>
2019-07-27 20:05:12 +0200

    fix

--- test/to.ts
+++ test/to.ts
@@ -1,11 +1,7 @@
 import test from 'ava';
 
-import {toDate, toTime} from '../dist';
+import {toDate} from '../dist';
 
 test('to date', t => {
 	t.is(toDate(1564146357), '7/26/2019');
-});
-
-test('to time', t => {
-	t.is(toTime(1564146357), '3:05:57 PM');
 });
